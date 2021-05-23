f890373b4bb8840be0c7d9558e1e5f701052ae47
satishkumar403 <satishkumarpapineni.dk@gmail.com>
2021-05-23 05:05:16 +0200

    Navigating to Twitter

---
+++ src/main/java/com/org/OrangeHrmTwitter.java
@@ -0,0 +1,31 @@
+package com.org;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrangeHrmTwitter {
+	@Test
+	public void twitter()
+	{
+		System.setProperty("webdriver.chrome.driver", ".//drivers/chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.findElement(By.xpath("//div[@id='social-icons']/a[3]")).click();
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		String parent =it.next();
+		String child=it.next();
+		driver.switchTo().window(child);
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//span[text()='Follow']")).click();
+		
+	}
+
+}
