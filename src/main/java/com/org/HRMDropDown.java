ec6d15ba170b525f1311ad4a8fcf17dbaeae11b0
bhanud <bhanud7111@example.com>
2021-05-27 08:01:07 +0530

    DropDownClass

--- src/main/java/com/org/HRMDropDown.java
+++ src/main/java/com/org/HRMDropDown.java
@@ -1,5 +1,20 @@
 package com.org;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class HRMDropDown {
+	
+	@Test
+	public void LoginPageDetailsTest()
+	{
+		System.setProperty("webdriver.chrome.driver", ".//drivers/chromedriver.exe");
+		WebDriver  driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		driver.manage().window().maximize();
+	}
 
 }
